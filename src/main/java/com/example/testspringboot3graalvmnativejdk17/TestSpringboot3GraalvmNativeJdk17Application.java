5fdceda351598b73d8606e2254cdc7c7b5ade150
makewheels <33256842+makewheels@users.noreply.github.com>
2022-12-05 11:24:17 +0800

    init

---
+++ src/main/java/com/example/testspringboot3graalvmnativejdk17/TestSpringboot3GraalvmNativeJdk17Application.java
@@ -0,0 +1,13 @@
+package com.example.testspringboot3graalvmnativejdk17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringboot3GraalvmNativeJdk17Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringboot3GraalvmNativeJdk17Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testspringboot3graalvmnativejdk17/TestSpringboot3GraalvmNativeJdk17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testspringboot3graalvmnativejdk17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringboot3GraalvmNativeJdk17ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
